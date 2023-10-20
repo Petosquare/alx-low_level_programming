5ae1ee3227ef1ca869958a98adf22c51e6bee969
Petosquare <etenungpeter@yahoo.com>
2023-10-19 17:17:56 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * ra - reverse array of integers
+ * reverse_array - reverse array of integers
  * @a: array
  * @n: number of elements of array
  *
  * Return: void
  */
-void ra(int *a, int n)
+void reverse_array(int *a, int n)
 {
 	int i;
 	int t;
